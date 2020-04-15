ef98ce6c4c22df414aa578fde5b4e508717bc72d
bot11x11 <procom@mail.ru>
2020-04-15 20:33:55 +0700

    the new page content

--- src/pages/new.vue
+++ src/pages/new.vue
@@ -2,8 +2,58 @@
   <Layout>
 
 <div class="container mx-auto">
-	<h1>A new page</h1>
+	<div class="flex mb-4">
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
 </div>
+		</div>
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
+</div>
+		</div>
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
+</div>
+		</div>
+	</div>
+</div>
+
   </Layout>
 </template>
 
