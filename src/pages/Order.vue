51d1e627626a84c374cd1d1d169c5211f9980c1a
bot11x11 <procom@mail.ru>
2020-04-16 16:48:32 +0700

    transfer

---
+++ src/pages/Order.vue
@@ -0,0 +1,97 @@
+<template>
+  <Layout>
+
+<div class="container mx-auto">
+	<div class="flex mb-4">
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">Transfer from / to the airport</div>
+    <p class="text-gray-700 text-base">
+      Book up to : 1 hour before<br />
+      Up to 7 people
+    </p>
+  </div>
+  <div class="flex">
+    <div class="w-1/2">Price : 175 &euro;</div>
+    <div class="w-1/2">Available</div>
+  </div>
+</div>
+		</div>
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
+</div>
+		</div>
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
+</div>
+		</div>
+	</div>
+</div>
+
+<div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+        Forgot Password?
+      </a>
+    </div>
+  </form>
+  <p class="text-center text-gray-500 text-xs">
+    &copy;2020 testForm.
+  </p>
+</div>
+
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'New'
+  }
+}
+</script>
+
