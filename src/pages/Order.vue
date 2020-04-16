38a55214c8095edc4c9e2302c680df7f278f64b2
bot11x11 <procom@mail.ru>
2020-04-16 16:52:09 +0700

    green

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -12,26 +12,10 @@
       Book up to : 1 hour before<br />
       Up to 7 people
     </p>
-  </div>
-  <div class="flex">
-    <div class="w-1/2">Price : 175 &euro;</div>
-    <div class="w-1/2">Available</div>
-  </div>
-</div>
-		</div>
-		<div class="w-1/3 px-2">
-<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
-  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 py-4">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    <div class="flex">
+      <div class="w-1/2">Price : 175 &euro;</div>
+      <div class="w-1/2 text-green-700">Available</div>
+    </div>
   </div>
 </div>
 		</div>
@@ -39,15 +23,31 @@
 <div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
   <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
   <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <div class="font-bold text-xl mb-2">Transfer from / to the airport</div>
     <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      Book up to : 1 hour before<br />
+      Up to 7 people
     </p>
+    <div class="flex">
+      <div class="w-1/2">Price : 175 &euro;</div>
+      <div class="w-1/2 text-green-700">Available</div>
+    </div>
   </div>
+</div>
+		</div>
+		<div class="w-1/3 px-2">
+<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+  <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
   <div class="px-6 py-4">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    <div class="font-bold text-xl mb-2">Transfer from / to the airport</div>
+    <p class="text-gray-700 text-base">
+      Book up to : 1 hour before<br />
+      Up to 7 people
+    </p>
+    <div class="flex">
+      <div class="w-1/2">Price : 175 &euro;</div>
+      <div class="w-1/2 text-green-700">Available</div>
+    </div>
   </div>
 </div>
 		</div>
