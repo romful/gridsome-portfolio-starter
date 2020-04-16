583d5084857cd61143a8c626596390f54c74ef42
bot11x11 <procom@mail.ru>
2020-04-16 17:10:09 +0700

    1

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -4,7 +4,7 @@
 <div class="container mx-auto">
 	<div class="flex mb-4">
 		<div class="w-1/3 px-2">
-<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+<div class="max-w-sm rounded overflow-hidden shadow-md mx-auto">
   <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">Transfer from / to the airport</div>
@@ -14,13 +14,13 @@
     </p>
     <div class="flex">
       <div class="w-1/2 text-700">Price : 175 &euro;</div>
-      <div class="w-1/2 text-green-700-right">Available</div>
+      <div class="w-1/2 text-green-700 text-right">Available</div>
     </div>
   </div>
 </div>
 		</div>
 		<div class="w-1/3 px-2">
-<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+<div class="max-w-sm rounded overflow-hidden shadow-md mx-auto">
   <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">Transfer from / to the airport</div>
@@ -30,13 +30,13 @@
     </p>
     <div class="flex">
       <div class="w-1/2 text-700">Price : 175 &euro;</div>
-      <div class="w-1/2 text-green-700-right">Available</div>
+      <div class="w-1/2 text-green-700 text-right">Available</div>
     </div>
   </div>
 </div>
 		</div>
 		<div class="w-1/3 px-2">
-<div class="max-w-sm rounded overflow-hidden shadow-lg mx-auto">
+<div class="max-w-sm rounded overflow-hidden shadow-md mx-auto">
   <img class="w-full" src="../../static/avatar.jpg" alt="Avatar" />
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">Transfer from / to the airport</div>
@@ -46,7 +46,7 @@
     </p>
     <div class="flex">
       <div class="w-1/2 text-700">Price : 175 &euro;</div>
-      <div class="w-1/2 text-green-700-right">Available</div>
+      <div class="w-1/2 text-green-700 text-right">Available</div>
     </div>
   </div>
 </div>
@@ -55,32 +55,39 @@
 </div>
 
 <div class="w-full max-w-xs mx-auto">
+  <h1 class="font-bold text-xl mb-2">Fill in the form below</h1>
+  <h2 class="font-bold text-xl mb-2">Transfer from / to the airport</h2>
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="date">
+        Date
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" type="text" placeholder="Input Date" />
     </div>
-    <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="time">
+        Time
       </label>
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-      <p class="text-red-500 text-xs italic">Please choose a password.</p>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="time" type="text" placeholder="Input Time" />
     </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="number">
+        How many people
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="number" type="text" placeholder="Input Number" />
+    </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="message">
+        Message
+      </label>
+      <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" placeholder="Input Message"></textarea>
+    </div>
+    <div class="flex items-center">
+      <button class="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Order
       </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
-      </a>
     </div>
   </form>
-  <p class="text-center text-gray-500 text-xs">
-    &copy;2020 testForm.
-  </p>
 </div>
 
 
