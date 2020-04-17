456dcb72097d69cf931d4d0d03bd66c97cabe7c0
bot11x11 <procom@mail.ru>
2020-04-17 18:18:03 +0700

    1

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -21,32 +21,32 @@
 				</div>
 			</div>
 			<div class="w-full max-w-xs mx-auto">
-				<h1 class="font-bold text-xl mb-2 text-center">Fill in the form below</h1>
-				<h2 class="font-bold text-lg mb-2 text-center">Transfer from / to the airport</h2>
-				<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+				<h1 class="font-bold text-xl mb-2 text-center">Fill in the form below :</h1>
+				<h2 class="text-md mb-2 text-center bg-blue-900 rounded-xl">Transfer from / to the airport</h2>
+				<form class="bg-white rounded-xl px-8 pt-6 pb-8 mb-4">
 					<div class="mb-4">
 						<label class="block text-gray-700 text-sm font-bold mb-2" for="date">
 							Date
 						</label>
-						<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" type="text" placeholder="Input Date" />
+						<input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="date" type="text" placeholder="Input Date" />
 					</div>
 					<div class="mb-4">
 						<label class="block text-gray-700 text-sm font-bold mb-2" for="time">
 							Time
 						</label>
-						<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="time" type="text" placeholder="Input Time" />
+						<input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="time" type="text" placeholder="Input Time" />
 					</div>
 					<div class="mb-4">
 						<label class="block text-gray-700 text-sm font-bold mb-2" for="number">
 							How many people
 						</label>
-						<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="number" type="text" placeholder="Input Number" />
+						<input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="number" type="text" placeholder="Input Number" />
 					</div>
 					<div class="mb-4">
 						<label class="block text-gray-700 text-sm font-bold mb-2" for="message">
 							Message
 						</label>
-						<textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" placeholder="Input Message"></textarea>
+						<textarea class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" placeholder="Input Message"></textarea>
 					</div>
 					<div class="text-center">
 						<button class="border-1 border-green-700 bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
