36684691bde861704bab2eff4e8d3b5a43558f72
Romain <romful@gmail.com>
2020-04-23 15:50:47 +0200

    test change

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,7 @@
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
     <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      Lorem ipsum dolor sit amet, consectetur aje testetdipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
   <div class="px-6 py-4">
