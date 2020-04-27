d44f4d5d74d8d5255f8338c360fb7d1a312f925f
bot11x11 <procom@mail.ru>
2020-04-27 17:02:02 +0700

    date

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -255,8 +255,6 @@ export default{
 		}
 	}
 }
-</script>
-<script>
 	const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 function app() {
