0d5ee6d185b80538c064b4ad66a1d72ca079d23a
bot11x11 <procom@mail.ru>
2020-04-28 20:59:34 +0700

    Vue.use(VueTimepicker)

--- src/main.js
+++ src/main.js
@@ -23,6 +23,8 @@ export default function (Vue, { router, head, isClient }) {
 
 /*  Vue.use(VueTailwindDatepicker)*/
 
+  Vue.use(VueTimepicker)
+
   head.meta.push({
     name: 'keywords',
     content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
