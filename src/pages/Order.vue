9c54db470d879accb56fd55f331b5f7d981f518b
bot11x11 <procom@mail.ru>
2020-04-28 20:12:55 +0700

    import './../vue2-timepicker/dist/VueTimepicker.css'

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -100,13 +100,14 @@
 				</form>
 			</div>
 		</div>
+import './../vue2-timepicker/dist/VueTimepicker.css'
 	</Layout>
 </template>
 
 <script>
 import VueTailwindDatepicker from '@coding-wisely/vue-tailwind-datepicker'
 import VueTimepicker from 'vue2-timepicker'
-import 'vue2-timepicker/dist/VueTimepicker.css'
+import './../vue2-timepicker/dist/VueTimepicker.css'
 export default{
 	components: {
 		VueTailwindDatepicker,
