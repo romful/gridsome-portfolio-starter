04b14116d63725599a589bc1e0c48722aea9279c
bot11x11 <procom@mail.ru>
2020-05-11 18:18:22 +0700

    order page with no auth

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -123,17 +123,6 @@
 				</form>
 			</div>
 		</div>
-  <div>
-    <div>
-      <img :src="$auth.user.picture" />
-      <h2>{{ $auth.user.name }}</h2>
-      <p>{{ $auth.user.email }}</p>
-    </div>
-
-    <div>
-      <pre>{{ JSON.stringify($auth.user, null, 2) }}</pre>
-    </div>
-  </div>
 	</Layout>
 </template>
 <script>
