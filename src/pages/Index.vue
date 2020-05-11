4ff14c27d6e309e7e887ba4c46c3e491dda761f4
Romain <romful@gmail.com>
2020-05-11 19:36:31 +0200

    update images

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,7 @@
 			<div class="mx-auto my-10 w-64 text-center uppercase font-bold">
 				{{ now }}
 			</div>
-			<h1 class="text-center">booking available up to 1 hour before the requested service</h1>
+			<h1 class="text-center">booking available up to 2 hour before the requested service</h1>
 			<div class="mx-auto my-10 relative w-64">
 				<select v-model="category" class="block appearance-none w-full border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
 					<option value="">-- select the category --</option>
