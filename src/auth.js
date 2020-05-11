d6422ad8d40bab4301a3e55cff3146758335500d
bot11x11 <procom@mail.ru>
2020-05-11 15:42:24 +0700

    https://www.storyblok.com/tp/how-to-auth0-vuejs-authentication

---
+++ src/auth.js
@@ -0,0 +1,97 @@
+import auth0 from 'auth0-js'
+import Vue from 'vue'
+
+// exchange the object with your own from the setup step above.
+let webAuth = new auth0.WebAuth({
+  domain: 'dev-nl95w9ph.eu.auth0.com',
+  clientID: 'LuEzNfO8PKMOUgAOTDyrinRxEJxljdUc',
+  // make sure this line is contains the port: 8080
+  redirectUri: 'https://modest-lewin-bd546d.netlify.app/login/',
+  // we will use the api/v2/ to access the user information as payload
+  audience: 'https://' + 'your_auth0_domain' + '/api/v2/', 
+  responseType: 'token id_token',
+  scope: 'openid profile' // define the scopes you want to use
+})
+
+let auth = new Vue({
+  computed: {
+    token: {
+      get: function() {
+        return localStorage.getItem('id_token')
+      },
+      set: function(id_token) {
+        localStorage.setItem('id_token', id_token)
+      }
+    },
+    accessToken: {
+      get: function() {
+        return localStorage.getItem('access_token')
+      },
+      set: function(accessToken) {
+        localStorage.setItem('access_token', accessToken)
+      }
+    },
+    expiresAt: {
+      get: function() {
+        return localStorage.getItem('expires_at')
+      },
+      set: function(expiresIn) {
+        let expiresAt = JSON.stringify(expiresIn * 1000 + new Date().getTime())
+        localStorage.setItem('expires_at', expiresAt)
+      }
+    },
+    user: {
+      get: function() {
+        return JSON.parse(localStorage.getItem('user'))
+      },
+      set: function(user) {
+        localStorage.setItem('user', JSON.stringify(user))
+      }
+    }
+  },
+  methods: {
+    login() {
+      webAuth.authorize()
+    },
+    logout() {
+      return new Promise((resolve, reject) => { 
+        localStorage.removeItem('access_token')
+        localStorage.removeItem('id_token')
+        localStorage.removeItem('expires_at')
+        localStorage.removeItem('user')
+        webAuth.logout({
+          returnTo: 'https://modest-lewin-bd546d.netlify.app/', // Allowed logout URL listed in dashboard
+          clientID: 'your_auth0_client_id', // Your client ID
+        })
+      })
+    },
+    isAuthenticated() {
+      return new Date().getTime() < this.expiresAt
+    },
+    handleAuthentication() {
+      return new Promise((resolve, reject) => {  
+        webAuth.parseHash((err, authResult) => {
+
+          if (authResult && authResult.accessToken && authResult.idToken) {
+            this.expiresAt = authResult.expiresIn
+            this.accessToken = authResult.accessToken
+            this.token = authResult.idToken
+            this.user = authResult.idTokenPayload
+            resolve()
+
+          } else if (err) {
+            this.logout()
+            reject(err)
+          }
+
+        })
+      })
+    }
+  }
+})
+
+export default {
+  install: function(Vue) {
+    Vue.prototype.$auth = auth
+  }
+}
--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import DefaultLayout from '~/layouts/Default.vue'
 import VueScrollTo from 'vue-scrollto'
 import VueFuse from 'vue-fuse'
 
-import { domain, clientId } from "../auth_config.json";
-import { Auth0Plugin } from "./auth";
+//import { domain, clientId } from "../auth_config.json";
+//import { Auth0Plugin } from "./auth";
+import { auth } from "./auth";
 
 import '~/css/style.css'
 export default function (Vue, { router, head, isClient }) {
@@ -23,7 +24,8 @@ export default function (Vue, { router, head, isClient }) {
 
   Vue.use(VueFuse)
 
-  Vue.use(Auth0Plugin, {
+  Vue.use(auth)
+/*  Vue.use(Auth0Plugin, {
     domain,
     clientId,
     onRedirectCallback: appState => {
@@ -33,7 +35,7 @@ export default function (Vue, { router, head, isClient }) {
           : window.location.pathname
       );
     }
-  });
+  });*/
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
