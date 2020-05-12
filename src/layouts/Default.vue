8f1e9abbeebceab1470cca6b16d2871279d395bb
bot11x11 <procom@mail.ru>
2020-05-12 15:40:50 +0700

    the menu

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="content-wrapper bg-background-primary font-sans text-copy-primary leading-normal flex flex-col min-h-screen" :class="theme">
-    <header class="bg-cover flex flex-col justify-around items-center" style="background-image: url(/portals.png); min-height: 42.67578125vw">
-      <img class="mt-10" src="/logo.png" />
+    <header class="bg-cover flex flex-col justify-around items-center" style="background-image: url(../../static/portals.png); min-height: 42.67578125vw">
+      <img class="mt-10" src="../../static/logo.png" />
       <div class="italic text-xl text-white">Concierge</div>
       <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
-        <div>
-          <g-link v-if="theme === 'theme-light'" to="/"><g-image src="../../static/logo.svg" class="w-40" alt="logo" /></g-link>
-          <g-link v-else to="/"><g-image src="../../static/logo_dark_mode.svg" class="w-40" alt="logo" /></g-link>
-        </div>
         <div class="block lg:hidden">
           <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
             <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
@@ -24,25 +20,22 @@
             <theme-switcher :theme="theme" @themeChanged="updateTheme" />
           </li>
           <li class="mr-8 mb-6 lg:mb-0">
-            <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-copy-primary hover:text-gray-600">Projects</a>
-            <g-link v-else to="/#projects" class="text-copy-primary hover:text-gray-600">Projects</g-link>
+            <a v-if="$route.path === '/'" href="/#projects" v-scroll-to="'#projects'" class="text-white hover:text-gray-600">Projects</a>
+            <g-link v-else to="/#projects" class="text-white hover:text-gray-600">Projects</g-link>
           </li>
           <li class="mr-8 mb-6 lg:mb-0">
-            <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-copy-primary hover:text-gray-600">About</a>
-            <g-link v-else to="/#about" class="text-copy-primary hover:text-gray-600">About</g-link>
+            <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-white hover:text-gray-600">About</a>
+            <g-link v-else to="/#about" class="text-white hover:text-gray-600">About</g-link>
           </li>
           <li class="mr-8 mb-6 lg:mb-0">
-            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-copy-primary hover:text-gray-600">Contact</a>
-            <g-link v-else to="/#contact" class="text-copy-primary hover:text-gray-600">Contact</g-link>
+            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-white hover:text-gray-600">Contact</a>
+            <g-link v-else to="/#contact" class="text-white hover:text-gray-600">Contact</g-link>
           </li>
           <li class="mr-8 mb-6 lg:mb-0">
-            <g-link to="/new" class="text-copy-primary hover:text-gray-600">New</g-link>
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0">
-            <g-link to="/docs" class="text-copy-primary hover:text-gray-600">Docs</g-link>
+            <g-link to="/docs" class="text-white hover:text-gray-600">Docs</g-link>
           </li>
           <li>
-            <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link>
+            <g-link to="/blog" class="text-white hover:text-gray-600">Blog</g-link>
           </li>
         </ul>
       </nav>
