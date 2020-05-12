8addc99b4fc9ff9b3866f32ad1d247bd103aec89
bot11x11 <procom@mail.ru>
2020-05-12 16:47:16 +0700

    page-query

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -293,24 +293,3 @@ export default{
 	}
 }
 </script>
-<page-query>
-query {
-	allGoogleSheet {
-		edges {
-			node {
-				Category
-				Featured
-				Title
-				Img
-				Price_vat_excluded
-				Booking_Up_to_X_hours
-				Up_to_X_pax
-				Price_per_hour
-				Price_per_people
-				Included
-				Availability
-			}
-		}
-	}
-}
-</page-query>
