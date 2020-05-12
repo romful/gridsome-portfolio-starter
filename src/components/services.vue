d6c48ad10042aa5eab3aa3e3b535f7453c6eefe6
bot11x11 <procom@mail.ru>
2020-05-12 17:18:53 +0700

    services

---
+++ src/components/services.vue
@@ -0,0 +1,21 @@
+<static-query>
+query {
+	allGoogleSheet {
+		edges {
+			node {
+				Category
+				Featured
+				Title
+				Img
+				Price_vat_excluded
+				Booking_Up_to_X_hours
+				Up_to_X_pax
+				Price_per_hour
+				Price_per_people
+				Included
+				Availability
+			}
+		}
+	}
+}
+</static-query>
