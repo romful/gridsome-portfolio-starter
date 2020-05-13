2442009bf722dec69fc7d54a480cabbd2c25a566
JSapp.me <contact@jsapp.me>
2020-05-13 12:22:57 -0600

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -47,7 +47,7 @@
     <footer class="bg-green-700 text-white">
       <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between py-8">
         <div class="mb-8 lg:mb-0">
-          <div>Copyright {{ new Date().getFullYear() }}. All rights reserved.</div>
+          <div>v.0.2.1 Copyright {{ new Date().getFullYear() }}. All rights reserved.</div>
           <div>
             <a href="/rss.xml" class="text-white hover:text-gray-400 font-normal">RSS Feed</a> |
             <a href="/sitemap.xml" class="text-white hover:text-gray-400 font-normal">Sitemap</a>
