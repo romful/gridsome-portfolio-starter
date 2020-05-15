c1f5c558e34a47d76423c580aa9ed3c0540d8f4e
bot11x11 <procom@mail.ru>
2020-05-15 19:25:45 +0700

    <d> to <div> for the description

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -159,7 +159,7 @@
 							</svg>
 						</div>
 					</div>
-					<p v-html="orderData.description"></p>
+					<div v-html="orderData.description"></div>
 					<div class="flex justify-end pt-2">
 						<button @click="showForm" class="border-1 text-white border-green-700 bg-blue-500 hover:bg-blue-700 font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline">Book now</button>
 					</div>
