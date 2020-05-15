e7a5522073a17fb29133968c5f7deced4a8aa67b
bot11x11 <procom@mail.ru>
2020-05-15 17:14:01 +0700

    modal fixed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -237,12 +237,12 @@ export default{
 	},
 	methods: {
 		toggleModal(){
-			const body = document.querySelector('body')
-			const modal = document.querySelector('.modal')
-			modal.classList.toggle('opacity-0')
-			modal.classList.toggle('pointer-events-none')
-			body.classList.toggle('modal-active')
-		}
+			const body = document.querySelector('body');
+			const modal = document.querySelector('.modal');
+			modal.classList.toggle('opacity-0');
+			modal.classList.toggle('pointer-events-none');
+			body.classList.toggle('modal-active');
+		},
 		dateFormat(date){
 			var MM, DD, YYYY;
 			var dtf = new Intl.DateTimeFormat('en', {
