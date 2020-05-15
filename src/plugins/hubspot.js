c59320cf0f17fe61749250b0aa2d9cae9613a8ee
bot11x11 <procom@mail.ru>
2020-05-15 15:50:54 +0700

    api.hubapi.com

--- src/plugins/hubspot.js
+++ src/plugins/hubspot.js
@@ -7,6 +7,25 @@ let hubspot = new Vue({
 	methods: {
 		getContactByEmail(email){
 			console.log(email);
+			fetch("https://api.hubapi.com/contacts/v1/contact/email/" + email + "/profile?hapikey=" + hapikey, {
+				method: "GET"
+			})
+			.then((response) => {
+				console.log(response);
+				return response.json();
+			})
+			.then((json) => {
+				console.log(json);
+				if(json.inlineMessage) {
+					alert(json.inlineMessage);
+				}else if(json.errors && json.errors[0] && json.errors[0].message){
+					alert(json.errors[0].message);
+				}else if(json.message){
+					alert(json.message);
+				}else{
+					alert("ERROR!!!");
+				}
+			});
 		}
 	}
 })
