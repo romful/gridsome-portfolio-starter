942d0abc46d6ce7f590d698cc9882f589ddf413a
bot11x11 <procom@mail.ru>
2020-05-15 17:09:14 +0700

    modal

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,7 @@
 				<g-link to="/profile">My Profile</g-link>
 			</div>
 			<div class="mx-auto my-10 w-64 text-center">
-				<button @click="hubspot.getContactByEmail(user.email)">Get my contact</button>
+				<button class="modal-open" @click="toggleModal">Open modal</button>
 			</div>
 			<div class="mx-auto my-10 w-64 text-center uppercase font-bold">
 				{{ now }}
@@ -150,6 +150,36 @@
 				</form>
 			</div>
 		</div>
+		<div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+			<div @click="toggleModal" class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+			<div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+				<div @click="toggleModal" class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+					<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+						<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+					</svg>
+					<span class="text-sm">(Esc)</span>
+				</div>
+				<div class="modal-content py-4 text-left px-6">
+					<div class="flex justify-between items-center pb-3">
+						<p class="text-2xl font-bold">Simple Modal!</p>
+						<div class="modal-close cursor-pointer z-50">
+							<svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+								<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+							</svg>
+						</div>
+					</div>
+					<p>Modal content can go here</p>
+					<p>...</p>
+					<p>...</p>
+					<p>...</p>
+					<p>...</p>
+					<div class="flex justify-end pt-2">
+						<button class="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action</button>
+						<button class="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</Layout>
 </template>
 <script>
@@ -183,6 +213,7 @@ export default{
 	},
 	data(){
 		return{
+			modalActive: false,
 			user: {},
 			category: "",
 			orderData: {
@@ -205,6 +236,13 @@ export default{
 		}
 	},
 	methods: {
+		toggleModal(){
+			const body = document.querySelector('body')
+			const modal = document.querySelector('.modal')
+			modal.classList.toggle('opacity-0')
+			modal.classList.toggle('pointer-events-none')
+			body.classList.toggle('modal-active')
+		}
 		dateFormat(date){
 			var MM, DD, YYYY;
 			var dtf = new Intl.DateTimeFormat('en', {
