e2d733235519298404c2d1a46314403f2a8942a0
bot11x11 <procom@mail.ru>
2020-05-15 17:32:51 +0700

    cross

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -153,12 +153,12 @@
 		<div class="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
 			<div @click="toggleModal" class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 			<div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
-				<button @click="toggleModal" class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+				<div @click="toggleModal" class="absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
 					<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
 						<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
 					</svg>
 					<span class="text-sm">(Esc)</span>
-				</button>
+				</div>
 				<div class="modal-content py-4 text-left px-6">
 					<div class="flex justify-between items-center pb-3">
 						<p class="text-2xl font-bold">Simple Modal!</p>
