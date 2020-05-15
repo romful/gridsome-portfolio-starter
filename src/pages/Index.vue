dbdb21f472bda9d6907d08b769cf776caa71bfb8
bot11x11 <procom@mail.ru>
2020-05-15 17:37:01 +0700

    same

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -159,6 +159,15 @@
 					</svg>
 					<span class="text-sm">(Esc)</span>
 				</div>
+				<div @click="toggleModal" class="top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+					<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+						<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+					</svg>
+					<span class="text-sm">(Esc)</span>
+				</div>
+				<div @click="toggleModal" class="absolute top-0 left-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+					1
+				</div>
 				<div class="modal-content py-4 text-left px-6">
 					<div class="flex justify-between items-center pb-3">
 						<p class="text-2xl font-bold">Simple Modal!</p>
