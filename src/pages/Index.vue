763ce113b3214f3025f9252a7821ec3ffa9dd33a
bot11x11 <procom@mail.ru>
2020-05-20 15:43:48 +0700

    title

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -171,6 +171,7 @@
 			<div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
 				<div class="modal-content py-4 text-left px-6">
 					<div class="flex justify-between items-center pb-3">
+						<p class="text-2xl font-bold text-center">Booking</p>
 						<div @click="toggleAlert" class="modal-close cursor-pointer z-50">
 							<svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
 								<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
