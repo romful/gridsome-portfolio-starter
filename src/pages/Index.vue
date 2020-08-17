865b85ad987f1784f8d25580d4ac8937eed8be45
bot11x11 <procom@mail.ru>
2020-08-17 20:02:12 +0700

    margin after service description

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,7 +23,7 @@
 			</div>
         			<div id="services" class="flex flex-wrap mb-4 max-w-5xl mx-auto" v-if="$services.category">
         				<h2 class="text-center w-full text-2xl font-bold">Choose a service</h2>
-        				<p class="text-center w-full">Click on any services to get additional informations</p>
+        				<p class="text-center w-full mb-4">Click on any services to get additional informations</p>
 				<div class="md:w-1/2 lg:w-1/3 w-full px-2" v-for="edge in $page.allGoogleSheet.edges" v-if="$services.category && edge.node.Category==$services.category">
 					<div @click="edge.node.Availability && init(edge.node)" class="mx-auto max-w-sm overflow-hiddenmx-auto cursor-pointer duration-500 transform hover:scale-110">
 						<div class="w-full rounded-xl service-image bg-no-repeat bg-cover bg-bottom" :style="{'background-image': 'url(' + edge.node.Img + ')' }"></div>
