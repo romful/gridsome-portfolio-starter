dc628a9331fc55d7166cc4f7716bcf564d8f7e62
bot11x11 <procom@mail.ru>
2020-08-17 16:37:07 +0700

    header

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="content-wrapper bg-background-primary font-sans text-copy-primary leading-normal flex flex-col min-h-screen" :class="theme">
-    <header class="relative md:flex flex-wrap justify-around items-center py-4">
+    <header class="relative flex justify-around items-center py-4 h-64 ms:h-auto ms:flex-col">
       <g-link class="relative text-white hover:text-gray-400" to="/">CONCIERGE</g-link>
-      <g-link class="relative" to="/"><img src="/logo.png" /></g-link>
+      <g-link class="relative" to="/"><img class="inline" src="/logo.png" /></g-link>
       <g-link class="relative text-white hover:text-gray-400" to="/profile">My account</g-link>
     </header>
 
